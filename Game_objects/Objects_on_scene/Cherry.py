6a27a7960ca3ecf10a152c3c83554759ae8c2658
Ратмир Жамьянов <frid23982@gmail.com>
2023-11-17 19:41:37 +0000

    Исправил мелкий недочет

--- Game_objects/Objects_on_scene/Cherry.py
+++ Game_objects/Objects_on_scene/Cherry.py
@@ -4,7 +4,7 @@ from pacman_developer.Game_objects.Objects_on_scene.Base_file_for_objects import
 
 class Cherry(Create_Object):
     def __init__(self, path: str, rect: pyray.Rectangle, weight: int) -> None:
-        super().__init__(path)
+        super().__init__(path, rect)
         self.weight = weight
         self.eaten = False
         self.radius = 0
