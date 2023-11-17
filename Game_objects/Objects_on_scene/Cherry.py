666da23a44a663ed73ff2f56ae95f3d4fce548ce
Ратмир Жамьянов <frid23982@gmail.com>
2023-11-17 19:31:42 +0000

    Добавил класс Cherry

---
+++ Game_objects/Objects_on_scene/Cherry.py
@@ -0,0 +1,15 @@
+import pyray
+from raylib import colors
+from pacman_developer.Game_objects.Objects_on_scene.Base_file_for_objects import Create_Object
+
+class Cherry(Create_Object):
+    def __init__(self, path: str, rect: pyray.Rectangle, weight: int) -> None:
+        super().__init__(path)
+        self.weight = weight
+        self.eaten = False
+        self.radius = 0
+        self.coordinate = [rect.x, rect.y]
+    def show(self):
+        self.radius = 12
+    def hide(self):
+        self.radius = 0
