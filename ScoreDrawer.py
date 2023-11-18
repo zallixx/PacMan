e12549e7b5da2b34e0b65b10bae23c4bf808a634
Кирилл Семёнов <ksemenov200411@yandex.ru>
2023-11-18 09:25:39 +0000

    Create ScoreDrawer

---
+++ ScoreDrawer.py
@@ -0,0 +1,20 @@
+import pyray
+from raylib import colors
+
+
+class ScoreDrawer:  # (RecalculableText):
+
+    def __init__(self, score: int = 0) -> None:
+        self.score = score
+        self.font_size_score = 75
+
+    def draw(self):
+        pyray.draw_text(str(ScoreDrawer.score), 50, 10, self.font_size_score, colors.BLACK)
+
+    def ScoreChanges(self):
+        #self.score будет изменяться при съедании семечек.
+        pass
+
+
+if __name__ == '__main__':
+    main()
