0cccfd4e34dc92a02d52ed011688b920b47a562f
Alex <almoroz08@gmail.com>
2023-11-24 19:34:14 +0300

    Удалил classes_logiс_on_gamescene.py. Логику из этого файла перенёс в файл пакмана(Pacman.py). Изменил структуру кода в gamescene.py

--- Drawing_scenes/gamescene.py
+++ Drawing_scenes/gamescene.py
@@ -1,7 +1,7 @@
 import pyray
 from Drawing_scenes.scene import Scene
 from Field_obj_drawing.FieldDrawer import FieldDrawer
-from Game_objects.Logic_of_objects_on_gamescene.classes_logiс_on_gamescene import logic_of_pacman
+from Game_objects.Classes_of_objects_on_gamescene.Pacman import Pacman
 
 
 class GameScene(Scene):
@@ -9,8 +9,8 @@ class GameScene(Scene):
         super().__init__()
         self.game = game
         self.draw_field = FieldDrawer()
-        self.pacman_logic = logic_of_pacman()
-        # Создание pacman_logic на основе класса логики/передвижения пакмана на игровой сцене
+        self.pacman = Pacman("images/frog.png", pyray.Rectangle(400, 335, 18, 18))
+        # Создаем self.pacman на основе класса Pacman
 
     def process_input(self) -> None:
         from Drawing_scenes.pausescene import PauseScene
@@ -23,8 +23,8 @@ class GameScene(Scene):
     def draw(self) -> None:
         pyray.draw_text("Game Scene", 10, 10, 20, pyray.WHITE)  # Отрисовка текста Game Scene в левом верхнем углу
         self.draw_field.draw_field()  # Отрисовка поля
-        self.pacman_logic.draw()  # Отрисовка пакмана
-        self.pacman_logic.event(self.draw_field.list_of_teleport, self.draw_field.list_of_seeds,
-                                self.draw_field.list_of_energizer)  # Передвижение пакмана
-        self.pacman_logic.logic(self.draw_field.list_of_walls_rectangles)  # Логика пакмана
+        self.pacman.draw()  # Отрисовка пакмана
+        self.pacman.event(self.draw_field.list_of_teleport, self.draw_field.list_of_seeds,
+                          self.draw_field.list_of_energizer, self.pacman)  # Передвижение пакмана
+        self.pacman.logic(self.draw_field.list_of_walls_rectangles, self.pacman)  # Логика пакмана
         # TODO: Нужно сделать так, чтобы пакман мог есть..
--- Game_objects/Classes_of_objects_on_gamescene/Pacman.py
+++ Game_objects/Classes_of_objects_on_gamescene/Pacman.py
@@ -9,19 +9,69 @@ import pyray
 class Pacman(Create_Object):
     def __init__(self, path: str, rect: pyray.Rectangle) -> None:
         super().__init__(path, rect)
+        self.pacman = None
+        # TODO: Текстуры
 
-    def event(self) -> None:  # Описывается движение пакмана
-        key_w = pyray.KeyboardKey.KEY_W
-        key_a = pyray.KeyboardKey.KEY_A
-        key_s = pyray.KeyboardKey.KEY_S
-        key_d = pyray.KeyboardKey.KEY_D
-        if pyray.is_key_down(key_w):
+    def event(self, list_of_teleports: list, list_of_seeds: list,
+              list_of_energizer: list, pacman) -> None:  # Описывается движение пакмана
+        self.pacman = pacman
+        if pyray.is_key_down(pyray.KeyboardKey.KEY_W):
             self.coordinate[1] -= 1
-        if pyray.is_key_down(key_s):
+        if pyray.is_key_down(pyray.KeyboardKey.KEY_S):
             self.coordinate[1] += 1
-        if pyray.is_key_down(key_a):
-            if self.coordinate[0] > 156:
-                self.coordinate[0] -= 1
-        if pyray.is_key_down(key_d):
-            if self.coordinate[0] < 646:
-                self.coordinate[0] += 1
+        if pyray.is_key_down(pyray.KeyboardKey.KEY_A):
+            self.coordinate[0] -= 1
+        if pyray.is_key_down(pyray.KeyboardKey.KEY_D):
+            self.coordinate[0] += 1
+
+        for i in range(len(list_of_teleports)):
+            teleport_rect = pyray.Rectangle(list_of_teleports[i][0], list_of_teleports[i][1],
+                                            list_of_teleports[i][2],
+                                            list_of_teleports[i][3])
+            pacman_rect = pyray.Rectangle(self.pacman.coordinate[0] - self.pacman.width / 2,
+                                          self.pacman.coordinate[1] - self.pacman.height / 2, self.pacman.width,
+                                          self.pacman.height)
+
+            if pyray.check_collision_recs(teleport_rect, pacman_rect):
+                if i == 0:
+                    self.pacman.coordinate = [634-36, 272]
+                else:
+                    self.pacman.coordinate = [148+36, 272]
+
+    def logic(self, walls_rectangles: list, pacman) -> None:
+        # Да.. данная функция крайне не понятна. Что ж, постараюсь объяснить
+        self.pacman = pacman
+        for i in range(0,
+                       len(walls_rectangles)):  # Цикл по pyray.Rectangle в списке walls_rectangles который создается в Cell_class.py
+            cube_rect = pyray.Rectangle(walls_rectangles[i][0], walls_rectangles[i][1], walls_rectangles[i][2],
+                                        walls_rectangles[i][
+                                            3])  # Создание pyray.Rectangle на основе x, y, self.s, self.s(self.s = 18)
+            pacman_rect = pyray.Rectangle(self.pacman.coordinate[0] - self.pacman.width / 2,
+                                          self.pacman.coordinate[1] - self.pacman.height / 2, self.pacman.width,
+                                          self.pacman.height)  # Создание pyray.Rectangle на основе x(пакмана), y(пакмана),
+            # self.pacman.width(18), self.pacman.height(18)
+
+            if pyray.check_collision_recs(cube_rect, pacman_rect):
+                overlap_x = min(pacman_rect.x + pacman_rect.width, cube_rect.x + cube_rect.width) - max(pacman_rect.x,
+                                                                                                        cube_rect.x)
+                overlap_y = min(pacman_rect.y + pacman_rect.height, cube_rect.y + cube_rect.height) - max(pacman_rect.y,
+                                                                                                          cube_rect.y)
+                # Эти две переменные представляют собой размер области,
+                # где прямоугольники пересекаются по горизонтали и вертикали соответственно
+
+                if overlap_x < overlap_y:
+                    # Если перекрытие по оси x меньше, чем по оси y, значит коллизия произошла горизонтально
+                    if pacman_rect.x < cube_rect.x:  # Проверка на центр пакмана и центр блока по y
+                        # Cмещаем пакмана влево
+                        self.pacman.coordinate[0] -= overlap_x
+                    else:
+                        # Cмещаем пакмана вправо
+                        self.pacman.coordinate[0] += overlap_x
+                else:
+                    # Коллизия произошла вертикально
+                    if pacman_rect.y < cube_rect.y:  # Проверка на центр пакмана и центр блока по y
+                        # Смещаем пакмана вверх
+                        self.pacman.coordinate[1] -= overlap_y
+                    else:
+                        # Cмещаем пакмана вниз
+                        self.pacman.coordinate[1] += overlap_y
