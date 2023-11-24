10476b4c6c23caab0e22911fe8435d52056bc2d8
GlebRGz <vintokryl5@yandex.ru>
2023-11-24 21:54:16 +0300

    Добавлена отрисовка счета

--- Drawing_scenes/gamescene.py
+++ Drawing_scenes/gamescene.py
@@ -3,13 +3,14 @@ from Drawing_scenes.scene import Scene
 from Field_obj_drawing.FieldDrawer import FieldDrawer
 from Game_objects.Classes_of_objects_on_gamescene.Pacman import Pacman
 from Game_objects.audio import Audio
-
+from pacman_developer.ScoreDrawer import ScoreDrawer
 
 class GameScene(Scene):
     def __init__(self, game) -> None:
         super().__init__()
         self.game = game
         self.draw_field = FieldDrawer()
+        self.score_draw = ScoreDrawer()
         self.pacman = Pacman("images/sprites/pacmanup.png", pyray.Rectangle(400, 335, 18, 18))
         self.start_audio = Audio(game)
         self.start_audio.play_track()
@@ -25,8 +26,10 @@ class GameScene(Scene):
 
     def draw(self) -> None:
         pyray.draw_text("Game Scene", 10, 10, 20, pyray.WHITE)  # Отрисовка текста Game Scene в левом верхнем углу
+
         self.draw_field.draw_field()  # Отрисовка поля
         self.pacman.draw()  # Отрисовка пакмана
+        self.score_draw.draw()  # Отрисовка счета
         self.pacman.event(self.draw_field.list_of_teleport, self.draw_field.list_of_seeds,
                           self.draw_field.list_of_energizer)  # Передвижение пакмана
         self.pacman.logic(self.draw_field.list_of_walls_rectangles)  # Логика пакмана
--- ScoreDrawer.py
+++ ScoreDrawer.py
@@ -6,10 +6,10 @@ class ScoreDrawer:  # (RecalculableText):
 
     def __init__(self, score: int = 0) -> None:
         self.score = score
-        self.font_size_score = 75
+        self.font_size_score = 50
 
     def draw(self):
-        pyray.draw_text(str(ScoreDrawer.score), 50, 10, self.font_size_score, colors.BLACK)
+        pyray.draw_text(str(self.score), 10, 30, self.font_size_score, colors.WHITE)
 
     def ScoreChanges(self):
         #self.score будет изменяться при съедании семечек.
