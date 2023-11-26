663f7aabbec5d144008410ddadb6c159bb352fe2
Alex <almoroz08@gmail.com>
2023-11-26 16:52:17 +0300

    cells.py, Cherry.py получили новые комментарии(некоторые прежние комментарии обновились)

--- objects/Cherry.py
+++ objects/Cherry.py
@@ -2,17 +2,39 @@ import pyray
 
 from objects.texture import Image
 
+# Импортим класс(Image) для создания объектов из
+# texture.py(.../objects/texture.py)
+# Для получения большей информации о классе - перейдите в файл
+
 
 class Cherry(Image):
-    def __init__(self, game, textures, rect: pyray.Rectangle, weight: int) -> None:
-        super().__init__(game, textures, rect)
+    def __init__(self, game, texture: pyray.Texture, rect: pyray.Rectangle, weight: int) -> None:
+        """ Класс вишенки, позволяющий работать с их отрисовкой и появлением
+        :param game: все переменные игры
+        :type game: Game
+        :param texture: текстура
+        :type texture: pyray.Texture
+        :param rect: положение, длина и ширина
+        :type rect: pyray.Rectangle
+        :param weight: вес вишенки
+        :type weight: int
+        """
+        super().__init__(game, texture, rect)
         self.weight = weight
         self.eaten = False
         self.radius = 0
         self.coordinate = [rect.x, rect.y]
 
-    def show(self):
+    def show(self) -> None:
+        """ Функция, назначающая радиус вишенки, то есть 12
+        :return: Null
+        """
         self.radius = 12
 
-    def hide(self):
+    def hide(self) -> None:
         self.radius = 0
+        """ Функция, которая прячет вишенку
+        :return: Null
+        """
+
+        # TODO: Не думаю, что данный класс должен выглядеть так. Во-первых появление вишенки надо бы сделать через также, как у seed(energizer). А удаление вишенки - съеданием пакмана
