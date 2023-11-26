3beab281c118fbf9764152214391c6016f4a9221
Alex <almoroz08@gmail.com>
2023-11-26 22:40:15 +0300

    Изменил создание объекта класса Text в ScoreDrawer. Теперь там RecalculableText(как и должно быть..).

--- ScoreDrawer.py
+++ ScoreDrawer.py
@@ -1,6 +1,6 @@
 import pyray
 from raylib import colors
-from objects.text import Text
+from objects.text import RecalculableText
 
 
 class ScoreDrawer:
@@ -8,10 +8,10 @@ class ScoreDrawer:
     def __init__(self, score: int = 0) -> None:
         self.score = score
         self.font_size_score = 50
-        self.score_text_object = Text(str(self.score), 10, 30, self.font_size_score, pyray.WHITE)
+        self.score_text_object = RecalculableText("{}", 10, 30, self.font_size_score, pyray.WHITE)
 
     def draw(self):
-        self.score_text_object.text = str(self.score)
+        self.score_text_object.recreate_text(str(self.score), "{}")
         self.score_text_object.draw_text()
 
     def add(self, point):
