7a875eea46896bd88e6969bbd157b704ab728918
Alex <almoroz08@gmail.com>
2023-11-30 19:20:07 +0300

    Добавил комментарии в файле ScoreCount.py, ScoreDrawer.py, main.py, LifeDrawer.py, Lifecount.py

--- LifeDrawer.py
+++ LifeDrawer.py
@@ -4,16 +4,28 @@ from objects.Sprite import Sprite
 
 
 class LifeDrawer(Sprite):
-    def __init__(self, rect: pyray.Rectangle, lifecount=3, ) -> None:
+    def __init__(self, rect: pyray.Rectangle, lifecount=3) -> None:
+        """ Класс отрисовки жизней пакмана, который позволяет работать с их отрисовкой и удалением
+        :param rect: координаты для отрисовки жизней
+        :type rect: pyray.Rectangle
+        :param lifecount: кол-во жизней
+        :type lifecount: int
+        """
         self.lifecount = lifecount
         self.radius = 14
         self.coordinate = [rect.x, rect.y]
 
-    def draw(self):
+    def draw(self) -> None:
+        """ Функция отрисовки жизней
+        :return: Null
+        """
         for i in range(self.lifecount):
             pyray.draw_circle(int(self.coordinate[0]) + (i * 3 * self.radius), int(self.coordinate[1]), self.radius,
                               colors.YELLOW)
 
     # будет вызываться по нажатию на клавишу F
-    def remove(self):
+    def remove(self) -> None:
+        """ Функция убавления жизней у пакмана
+        :return: Null
+        """
         self.lifecount -= 1
--- Lifecount.py
+++ Lifecount.py
@@ -1,13 +1,23 @@
 class LifeCounter:
 
     def __init__(self, life_count=3):
+        """ Класс жизней пакмана, который позволяет работать с их добавлением и удалением
+        :param life_count: кол-во жизней
+        :type life_count: int
+        """
         self.life_count = life_count
 
-    def add(self):
+    def add(self) -> int:
+        """ Функция добавления жизней у пакмана
+        :return: int
+        """
         self.life_count += 1
         return self.life_count
 
-    def remove(self):
+    def remove(self) -> float:
+        """ Функция убавления жизней у пакмана
+        :return: float(or int)
+        """
         self.life_count -= 1
         if self.life_count < 0:
             return 1 / 0
